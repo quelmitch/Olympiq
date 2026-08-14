25ed0a55bb3b02372bbc78ad043c8e51ba26d263
Stefano Miceli <106555077+quelmitch@users.noreply.github.com>
2026-08-14 15:59:22 +0200

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { VitePWA } from 'vite-plugin-pwa';
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/Olympiq/',
     plugins: [
         react(),
         tailwindcss(),
